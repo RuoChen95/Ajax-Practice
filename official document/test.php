a9dd6019689c09987f2f242b17c62b4f49bb3f08
RuoChen95 <xrc_1995@163.com>
2017-03-28 21:54:27 +0800

    Add files via upload

---
+++ official document/test.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<?php
+$name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
+$computedString = "Hi, " . $name;
+$array = ['userName' => $name, 'computedString' => $computedString];
+echo json_encode($array);
+?>
+
+</body>
+</html>
+
+
